40e8bb02a2795675028905c314b2c13f91b07e9a
Alexander Shabanov <avshabanov@gmail.com>
2015-05-04 00:18:13 -0700

    Adding statement+expression

--- jb2c-cli/src/main/java/com/truward/jb2c/mid/Types.java
+++ jb2c-cli/src/main/java/com/truward/jb2c/mid/Types.java
@@ -1,6 +1,9 @@
 package com.truward.jb2c.mid;
 
+import com.sun.corba.se.spi.orbutil.fsm.State;
+
 import javax.annotation.Nonnull;
+import java.util.List;
 
 /**
  * @author Alexander Shabanov
@@ -8,21 +11,19 @@ import javax.annotation.Nonnull;
 public final class Types {
 
   interface Visitor<R> {
-    R visitNode(@Nonnull Node node);
-
-    default R visitPrimitiveType(@Nonnull PrimitiveType node) {
-      return visitNode(node);
+    default R visitNode(@Nonnull Node node) {
+      throw new UnsupportedOperationException("Unsupported node: " + node);
     }
+
+    default R visitPrimitiveType(@Nonnull PrimitiveType node) { return visitNode(node); }
+    default R visitClassType(@Nonnull ClassType node) { return visitNode(node); }
   }
 
   interface Node {
-    <R> R apply(@Nonnull Visitor<R> visitor);
+    <R> R apply(@Nonnull Visitor<R> v);
   }
 
   interface Type extends Node {
-    default String getName() {
-      return "Unknown";
-    }
   }
 
   enum PrimitiveTypeKind {
@@ -36,11 +37,54 @@ public final class Types {
   }
 
   interface PrimitiveType extends Type {
-    @Nonnull
-    PrimitiveTypeKind getKind();
+    @Nonnull PrimitiveTypeKind getKind();
 
-    default @Override <R> R apply(@Nonnull Visitor<R> visitor) {
-      return visitor.visitPrimitiveType(this);
-    }
+    default @Override <R> R apply(@Nonnull Visitor<R> v) { return v.visitPrimitiveType(this); }
+  }
+
+  interface ClassType extends Type {
+    default @Override <R> R apply(@Nonnull Visitor<R> v) { return v.visitClassType(this); }
+  }
+
+  //
+  // Expressions
+  //
+
+  interface Expression extends Node {
+  }
+
+  interface AnnotationExpression extends Expression {
+    @Nonnull Type getAnnotationType();
+
+    @Nonnull List<Expression> getArguments();
+  }
+
+  //
+  // Statements
+  //
+
+  interface Statement extends Node {
+    long getAccessFlags();
+
+    @Nonnull List<AnnotationExpression> getAnnotations();
+  }
+
+  // TODO: util:
+  static boolean isPublic(@Nonnull Statement statement) {
+    return (statement.getAccessFlags() & 0x1) != 0;
+  }
+
+  interface Var extends Statement {
+    @Nonnull Type getType();
+
+    @Nonnull String getName();
+  }
+
+  interface Method extends Statement {
+    @Nonnull Type getReturnType();
+
+    @Nonnull String getName();
+
+    @Nonnull List<Var> getParameters();
   }
 }
